54e0e0d2348d7fa33661ec02f5f940ceb419ceaa
Steven Cahail <scahail@utexas.edu>
2013-01-22 09:56:09 -0600

    server.js serves static index page, but page doesn't do anything (can only type in form)

--- server.js
+++ server.js
@@ -20,15 +20,16 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
+  app.use(app.router); //Running this with line 32 (app.get('/', routes.index);) breaks the UI
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/static')); //mine
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/tweet', tweet.getTweet);
 
